1278cfd8bcca75c8587876be7f18897ab4bad516
Sugar Duong <sugar.duong@itl.com>
2019-09-13 11:40:53 +0700

    complete EDIT, SEARCH, DELETE and add confirm modal

--- connect/index.php
+++ connect/index.php
@@ -1,4 +1,8 @@
 <?php
     $error = "";
     $db = mysqli_connect('localhost', 'root', '', 'todo');
+    if($db === false) {
+        die("ERROR: Could not connect. "
+            . $db->connect_error);
+    }
 ?>
--- edit/index.php
+++ edit/index.php
@@ -5,30 +5,38 @@
 ?>
 
 <?php
+    $id = "";
     if (isset($_GET['id'])) {
         $id = $_GET['id'];
     }
 
     if (isset($_POST['submit'])) {
+        $id = $_POST['id'];
         $task = $_POST['task'];
         $assign = $_POST['assign'];
         $email = $_POST['email'];
+        
         if (empty($task) || empty($assign) || empty($email)) {
             $error = "You must fill in all task";
         } else {
-            mysqli_query($db, "UPDATE tasks SET task='$task' assign='$assign' email='$email' WHERE id='$id'");
-            header('location: index.php');
+            mysqli_query($db, "UPDATE tasks SET task = '$task', assign = '$assign', email = '$email' WHERE id = '$id' ");
+            header('location: /todolist-php/main/index.php');
         }
     }
-    $task = mysqli_query($db, "SELECT * FROM tasks WHERE id='$id'")
+    $tasks = mysqli_query($db, "SELECT * FROM tasks WHERE id='$id'")
 ?>
 
 <body>
     <header>
         <div class="heading">
-            <h2>Sugar Edit TodoList with id = <?php echo $id?><h2>
-            <?php while ($row = mysqli_fetch_array($task)) { ?>
-                <form method="POST" action="/todolist-php/main/index.php">
+            
+            <?php while ($row = mysqli_fetch_array($tasks)) { ?>
+                <form method="POST" action="/todolist-php/edit/index.php">
+                <input style="display: none"
+                type="text"
+                name="id"
+                value= "<?php echo $id?>"
+                readonly>
                     <input type="text" 
                         name="task" 
                         class="task_input" 
